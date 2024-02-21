c655a4bbe9e2d57ff6c2b7ffe7a7179be1e425aa
peachez-z <122859326+peachez-z@users.noreply.github.com>
2024-02-21 14:55:54 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240221/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
+++ 240221/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
@@ -1,9 +1,10 @@
 num = list(map(int,input().split()))
 total = 0
+cnt = 0
 for i in range(len(num)):
-    
     if num[i] == 0 and i > 2:
-        if total == 0:
+        if cnt == 0:
+            cnt += 1
             total += num[i-1]
             total += num[i-2]
             total += num[i-3]
